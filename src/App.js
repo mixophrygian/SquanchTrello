e1ecf744795a40f3eeedf984fbe570b7d3334135
Eleanor Weigert <eleanor.weigert@byhook.com>
2017-02-15 23:28:53 -0800

    highlevel UI with material-ui and css reset

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+/* Material UI for Add button*/
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+/* Material UI for add-task modal */
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
   render() {
+
+    const actions = [
+      <FlatButton
+        label="Add Task"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App__addTask">
+          <div className="App__addTask--content">
+            <h1>ADD TASK</h1>
+            <div className="App__addTask--content button">
+              <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+                <FloatingActionButton onClick={this.handleOpen}>
+                  <ContentAdd />
+                </FloatingActionButton>
+              </MuiThemeProvider>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>TODO</div>
+        <div>IN PROGRESS</div>
+        <div>DONE</div>
+
+        <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+          <Dialog
+            title="Add a new Todo"
+            actions={actions}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+          >
+          <TextField
+            floatingLabelText="Task Name"
+            hintText="Task Name"
+            required
+          />
+          <TextField
+            floatingLabelText="Task Description"
+          />
+          </Dialog>
+        </MuiThemeProvider>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+/*Material UI requirement */
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
